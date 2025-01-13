03992181dc57598c491785b375c9b6c7f43630e2
Nathaniyell <essien.nathan@yahoo.com>
2025-01-13 11:33:50 +0100

    update data models

--- src/data_models/upcoming_events.js
+++ src/data_models/upcoming_events.js
@@ -4,21 +4,21 @@ import { headers } from "next/headers";
 
 export const upcomingEvents = [
   {
-    date: "FEBRUARY 1,2024",
+    date: "FEBRUARY 1,2025",
     top: "Publisher:",
     header: "Abstract/Full Paper Submission",
     body: "Effective collaboration between academia and industry is necessary to create a sustainable and transformative win-win situation for all stakeholders. ",
     bottom: "MAKE SUBMISSIONS TO: aiccees@uniport.edu.ng",
   },
   {
-    date: "AUGUST 31,2024",
+    date: "May 31,2025",
     top: "Publisher:",
     header: "Abstract/Full Paper Submission",
     body: "Effective collaboration between academia and industry is necessary to create a sustainable and transformative win-win situation for all stakeholders. ",
     bottom: "MAKE SUBMISSIONS TO: aiccees@uniport.edu.ng",
   },
   {
-    date: "SEPTEMBER 26 & 27,2024",
+    date: "SEPTEMBER 25 & 26, 2025",
     top: "Organized by:",
     header: "Abstract/Full Paper Submission",
     body: "Effective collaboration between academia and industry is necessary to create a sustainable and transformative win-win situation for all stakeholders. ",
